45302a6078b3d47faf2085c51ba706148a1c81f7
SeungBum Kim <picxenk@gmail.com>
2017-09-22 01:57:30 +0900

    show big mp image

--- webapp.js
+++ webapp.js
@@ -6,6 +6,7 @@ var shotDir = 'public_html/shot';
 
 var indexContent = '';
 var itemContent = '';
+var bigContent = '';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -46,13 +47,14 @@ app.get('/:id', (req, res) =>  {
             for (var i=0; i<items.length; i++) {
                 var n = i+1;
                 links += ' <a href="/'+n+'">'+n+'</a> ';
+                if ((i+1)%30 == 0) links += '<br/>';
             }
             links += ']';
             var result = itemContent.toString().replace('#NAV#', links);
 
             var id = req.params.id;
             var oriImage = '<img src="/shot/'+id+'/ori.png" width="400"/>';
-            var mpImage= '<img src="/shot/'+id+'/mp.png" width="400"/>';
+            var mpImage= '<a href="/big/'+id+'"><img src="/shot/'+id+'/mp.png" width="400"/></a>';
             var codeText = '';
 
             fs.readFile('public_html/shot/'+id+'/code.txt', (err, code) => {
@@ -65,6 +67,18 @@ app.get('/:id', (req, res) =>  {
         });
 });
 
+app.get('/big/:id', (req, res) =>  {
+        fs.readdir(shotDir, (err, items) => {
+
+            var id = req.params.id;
+            var result = bigContent.toString();
+
+            result = result.replace('#LINK#', id);
+            result = result.replace('#MPI#', id);
+            res.send(result);
+        });
+});
+
 
 app.use(express.static('public_html'));
 app.listen(80, () => {
@@ -74,5 +88,8 @@ app.listen(80, () => {
     });
     fs.readFile('public_html/item.html', (err, content) => {
         itemContent = content;
+    });
+    fs.readFile('public_html/big.html', (err, content) => {
+        bigContent = content;
     });
 });
